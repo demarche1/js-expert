aac6f177e53283323ad28866a4a3e1ebeeea9d71
demarche1 <alessandro.demarche00@gmail.com>
2022-06-06 18:13:30 -0300

    ref vs valor anotations

--- modulo02-conceitos-fundamentais-sobre-ciclo-de-vida-do-javascript-pt01/ref-vs-valor/index.js
+++ modulo02-conceitos-fundamentais-sobre-ciclo-de-vida-do-javascript-pt01/ref-vs-valor/index.js
@@ -1,7 +1,7 @@
 const {deepStrictEqual} = require('assert')
 
 let value = 0
-let value2 = value
+let value2 = value // => Vai para o Call Stack
 value2++
 // Tipos primitivos gera cópia em memória
 deepStrictEqual(value, 0)
@@ -10,7 +10,7 @@ deepStrictEqual(value2, 1)
 // Tipos de referência, copia o endereço de memória
 // e aponta para o mesmo lugar
 let obj = {value: 0}
-let obj2 = obj
+let obj2 = obj // => Armazena o valor de referência no Memory Heap
 
 obj.value++
 deepStrictEqual(obj, {value: 1})
